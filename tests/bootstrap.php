41e9ad99fcadc86640769fb4f12693093b8317fe
devThomas <thomas.boff.dev@gmail.com>
2023-09-01 09:36:45 +0700

    change link in the tests/bootstrap for travis ci

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,12 +7,8 @@
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 if ( ! $_tests_dir ) {
-  $_tests_dir = 'C:/xampp/htdocs/wordpress-tests-lib';
+  $_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
 }
-$_tests_dir = 'C:/xampp/htdocs/wordpress-tests-lib';
-
 
 // Load the main WordPress tests bootstrap file.
 require_once $_tests_dir . '/includes/bootstrap.php';
-
-require_once 'C:\xampp\htdocs\wordpress_plugin_test\wp-content\plugins\technical_assessment_wpmedia\technical_assessment_wpmedia.php';
